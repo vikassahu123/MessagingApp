63d705869fd3bef8aac69cdac7881fda29e8b325
vikassahu123 <135429562+vikassahu123@users.noreply.github.com>
2025-02-21 16:42:42 +0530

    Vikas Sahu UC3 name by parameter

---
+++ MessagingApp/src/test/java/com/example/UC1/Hello_uc1/HelloUc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UC1.Hello_uc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloUc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
