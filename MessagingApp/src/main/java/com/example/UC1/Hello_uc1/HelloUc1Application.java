f1b8a422c9fb4aaf883923727ea3a7f6f399504d
vikassahu123 <135429562+vikassahu123@users.noreply.github.com>
2025-02-21 13:24:01 +0530

    Vikas Sahu UC1_GET_hello problem

---
+++ MessagingApp/src/main/java/com/example/UC1/Hello_uc1/HelloUc1Application.java
@@ -0,0 +1,14 @@
+package com.example.UC1.Hello_uc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloUc1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HelloUc1Application.class, args);
+	}
+
+}
